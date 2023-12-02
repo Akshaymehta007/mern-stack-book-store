afc1c7177edd1b0e2d959c17405b14e44c981ed5
Akshaymehta007 <akshaymehta2107@gmail.com>
2023-12-02 12:42:40 +0530

    New deployment changes

--- Backend/app.js
+++ Backend/app.js
@@ -12,10 +12,13 @@ app.use(express.json())//This will convert the data into JSON format so that our
 app.use(cors());
 app.use("/books", router);
 
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, 'book-store', 'build')));
-app.get('*', (req,res) =>{
-    res.sendFile(path.join(__dirname, 'book-store', 'build', 'index.html'));
+const rootDir = path.resolve(__dirname);
+const buildDir = path.join(rootDir, 'book-store', 'build');
+
+app.use(express.static(buildDir));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(buildDir, 'index.html'));
 });
 
 mongoose.connect("mongodb+srv://admin:OACY3KWpz4JeEHcE@cluster0.taseyg5.mongodb.net/?retryWrites=true&w=majority").then(()=>console.log("Connected to Database"))
