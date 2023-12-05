1b1a6b52a32fcb5017e50e5a68c95c8b3bdf30e9
Akshaymehta007 <akshaymehta2107@gmail.com>
2023-12-05 09:56:40 +0530

    New Changes

--- Backend/app.js
+++ Backend/app.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const PORT = process.env.PORT || 5000;
 const mongoose = require('mongoose');
 const  router = require("./routes/book-routes");
 const cors = require('cors');
 const app = express();
 const path = require("path");
+const BASE_URL = process.env.BASE_URL;
 
 
 //Middleware
@@ -24,8 +24,8 @@ app.get('*', (req, res) => {
 mongoose.connect("mongodb+srv://admin:OACY3KWpz4JeEHcE@cluster0.taseyg5.mongodb.net/?retryWrites=true&w=majority").then(()=>console.log("Connected to Database"))
 .then(() => {
     console.log("Connected to Database");
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
+    app.listen(5000, 'localhost', () => {
+      console.log(`Server is running at ${BASE_URL}`);
     });
   })
   .catch((err) => console.log(err));
